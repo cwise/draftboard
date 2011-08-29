ea6f533a93fda2bbc30d5095bc41d1d58b3a9aed
Chris Wise <cwise@murmurinformatics.com>
2011-08-29 08:32:48 -0400

    added seeds for poolies and teams

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,8 +1,12 @@
+require 'csv'
+
 class Player < ActiveRecord::Base
   has_one :draft_pick
   scope :not_drafted, where("id NOT IN (SELECT player_id FROM draft_picks)")
   scope :drafted, where("id IN (SELECT player_id FROM draft_picks)")
   scope :position, lambda {|pos| where(['position = ?', pos])}
+  
+  VALID_POSITIONS=%w{QB RB WR TE K}
   
   def full_name
     "#{name} (#{position}/#{team})"
@@ -14,5 +18,30 @@ class Player < ActiveRecord::Base
   
   def selected?
     !draft_pick.nil?
+  end
+  
+  def self.import_csv filename
+    added=0
+    skipped=0
+
+    CSV.foreach(filename, {:col_sep => ",", :headers => :firstrow}) do |row|
+      Player.transaction do
+        team=row['TeamAbbName']
+        team.upcase!
+        
+        player_name=row['PlayerName']
+        all_positions=row['PlayerPosition'].split('/').map{|pos| pos=='FB' ? 'RB' : pos}.select{|pos| VALID_POSITIONS.include?(pos)}
+        position=all_positions.first
+        
+        if position
+          Player.create :name => player_name, :team => team, :position => position
+          added+=1
+        else
+          skipped+=1
+        end
+      end
+    end    
+    
+    "File #{filename} imported: added #{added} skipped #{skipped}"
   end
 end
