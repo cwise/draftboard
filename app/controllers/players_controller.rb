18721e4f28849163396b9971b1be732fae7547f7
Chris Wise <cwise@murmurinformatics.com>
2011-08-29 08:40:32 -0400

    changed order on player controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,6 +2,6 @@ class PlayersController < ApplicationController
   def index
     @position=params[:position]
     logger.info("position = #{@position}")
-    @players=Player.position(@position).includes(:draft_pick).all
+    @players=Player.position(@position).includes(:draft_pick).order('name').all
   end
 end
