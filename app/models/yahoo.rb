e3ac1ae69d0bb615beb38116f81efcabc677554c
Chris Wise <cwise@murmurinformatics.com>
2014-08-27 08:54:16 -0400

    updated upload

--- app/models/yahoo.rb
+++ app/models/yahoo.rb
@@ -1,12 +1,12 @@
 class Yahoo
   def self.read_players
-    (1..14).to_a.each do |index|  
+    (1..17).to_a.each do |index|  
       read_page index
     end
   end
   
   def self.read_page page
-    path = "/Users/cwise/Desktop/page#{page}.html"
+    path = "/Users/cwise/Downloads/page#{page}.html"
     doc = Nokogiri::HTML(open(path))
     nodes = doc.css('.ysf-player-name')
     nodes.each do |node|
